633bf8b3a7e7fb79fb53ea096c9613b7f829cb23
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-19 02:02:17 +0300

    commit tree 6d66d2bbb2b7085196144e54c93983c2c49672a1

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
 */
+// Runtime: 0(n^2)
+// Each for loop has multiple steps and they are nested loops
+// Not the best run time
+// Space: 0(n)
+// Linear
+// As needs of array grows, memory will grow in proportion
+// */
+
 
 /* 
   Add your pseudocode here
